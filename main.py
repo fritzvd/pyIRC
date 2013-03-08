77bad9fac7cebfb3a31bdb9781cab6981c5c8f47
YatharthROCK <yatharth999@gmail.com>
2013-03-08 17:17:45 +0300

    Add hello world index page

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 
 import os
 
 import webapp2
 import jinja2
-
-import pyirclogs
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -14,15 +12,10 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        path = './logs' + self.request.path
-        b = os.path.exists(path)
-        os.makedirs([])
-
         template_values = {}
 
-        self.response.out.write('<html><body>%s: %s</body></html>' % (path, str(b)))
-        #template = jinja_environment.get_template('templates/DirPage.html')
-        #self.response.out.write(template.render(template_values))
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/.*', MainPage)],
                               debug=True)
